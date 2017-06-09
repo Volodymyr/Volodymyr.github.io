fdd845650d7e112ad4b523006ed10391f21aada6
Volodymyr <vovazaikin3188@gmail.com>
2017-06-09 05:35:27 +0300

    drive-3

--- drive/js/main.js
+++ drive/js/main.js
@@ -23,17 +23,17 @@ $(document).ready(function(){
 	});
 
 	$('.sea-slider').slick({
-		slidesToShow: 4,
+		slidesToShow: 6,
 		autoplay: true,
 		autoplaySpeed: 2000,
 		dots: true,
-		slidesToScroll: 2,
+		slidesToScroll: 3,
 		responsive: [
 		{
 			breakpoint: 1282,
 			settings: {
 				arrows: false,
-				slidesToShow: 3
+				slidesToShow: 4
 			}
 		},
 		{
@@ -44,5 +44,53 @@ $(document).ready(function(){
 			}
 		}
 		]
+	});
+
+	$('.reviews-slider').slick({
+		slidesToShow: 6,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		dots: true,
+		slidesToScroll: 3,
+		responsive: [
+		{
+			breakpoint: 1282,
+			settings: {
+				arrows: false,
+				slidesToShow: 4
+			}
+		},
+		{
+			breakpoint: 1005,
+			settings: {
+				arrows: false,
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+
+	//Links
+
+	$(document).ready(function(){
+		$(".navbar-nav").on("click","a", function (event) {
+			//отменяем стандартную обработку нажатия по ссылке
+			event.preventDefault();
+
+			//забираем идентификатор бока с атрибута href
+			var id  = $(this).attr('href'),
+
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+			//анимируем переход на расстояние - top за 1500 мс
+			$('body,html').animate({scrollTop: top}, 1500);
+		});
+	});
+
+	//gallery
+
+	$('.grid').masonry({
+		itemSelector: '.grid-item'
 	});
 });
